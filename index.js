949f8fdfb310be15a7e55d11f415fb32caa32210
mbremyk <mbremyk@hotmail.no>
2021-04-13 23:28:02 +0200

    Remove tutorial files Add simple server thingy

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+let cors = require('cors');
+let app = express();
+let fs = require('fs');
+let expressip = require('express-ip');
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(expressip().getIpInfoMiddleware);
+app.use(cors());
+
+app.get("/", (req, res) => {
+    fs.readFile('index.html', 'utf-8', (err, data) => {
+        if (err) throw err;
+        data = data.replace('ipnr', req.headers['x-forwarded-for'] || req.ipInfo.ip).replace('portnr', req.connection.remotePort);
+        res.send(data);
+    })
+});
+
+app.get("/add", (req, res) => {
+	let a = Number(req.query.a), b = Number(req.query.b);
+	res.send("" + (a + b));
+});
+
+app.listen(PORT, '0.0.0.0',() => console.log(`Listening on ${ PORT }`));
