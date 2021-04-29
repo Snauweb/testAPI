d9508d9d85a8b29ea2392c6973ed298c13a44e4a
mbremyk <mbremyk@hotmail.no>
2021-04-29 20:51:31 +0200

    Add comments for fucked method

--- utils.js
+++ utils.js
@@ -10,7 +10,10 @@ const readObjectsFromFileAndParseQuery = (file, options) => {
         .then(file => file.readFile('utf-8'))
         .then(str => JSON.parse(str))
         .then(objects => {
+            //Remove reserved parameters like 'antall' 
             const keys = Object.keys(options).filter(key => !reservedParams.includes(key));
+            //Filter all objects that don't match query parameters
+            //String parameters are matched on substring, number parameters are matched on value
             keys.forEach(key => objects = objects.filter(object => {
                 if (typeof object[key] == 'number') return object[key] == options[key];
                 return ((String)(object[key])).includes(options[key]);
@@ -18,14 +21,18 @@ const readObjectsFromFileAndParseQuery = (file, options) => {
             return objects;
         })
         .then(objects => {
+            //Sort based on given key
+            //TODO sort ascending or descending dependent on preference
             if (options.sorter) {
                 const [key, order] = options.sorter.split('-');
+                key = key ? key : 'id';
                 return objects.sort((a, b) => {
                     return ((String)(a[key])).localeCompare((String)(b[key]));
                 });
             }
             return objects;
         })
+        //Get only as many objects as requested
         .then(objects => objects.slice(0, options.antall ? options.antall : objects.length));
 }
 
